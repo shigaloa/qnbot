44fb5ce91d2ea4d7ffc8a16d0a4f27b20e2bb0df
Francisco <shigaloa@gmail.comm>
2019-03-06 10:49:32 +0100

    commit desde vscode

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qnpruebabot-Web-Deploy.pubxml -p:Password=eit3LDWdnHqSsecuA975X61vJs2GXiA7b8FlqAhrd0J4CBeco006zXcedq9W
+
